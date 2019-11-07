76974b4e8f226594a96432a70616a6b99f2309f1
Antti Tulisalo <suolapeikko.developer@gmail.com>
2019-11-07 17:00:49 +0200

    Added Swift 5 support and fixed some bugs.

--- beacontool/BeaconBroadcaster.swift
+++ beacontool/BeaconBroadcaster.swift
@@ -13,32 +13,27 @@ class BeaconBroadcaster: NSObject, CBPeripheralManagerDelegate {
     
     var bluetoothPeripheralManager: CBPeripheralManager!
     var data: Beacon!
+    let beaconQueue:DispatchQueue = DispatchQueue(label: "com.suolapeikko.broadcastqueue", attributes: [])
 
     /// Starting with fixed iBeacon data
     override init() {
         
         super.init()
         
-        // Define serial dispatch queue
-        let beaconQueue:DispatchQueue = DispatchQueue(label: "com.suolapeikko.broadcastqueue", attributes: [])
-
         bluetoothPeripheralManager = CBPeripheralManager(delegate: self, queue: beaconQueue, options: nil)
         data = Beacon(uuid: UUID().uuidString, major: 65535, minor: 1, power: -58)
     }
 
-    /// Starting with attributed iBeacon data
+    /// Starting with custom iBeacon data
     init(uuid: String, major: Int, minor: Int, power: Int) {
         
         super.init()
-        
-        // Define serial dispatch queue
-        let beaconQueue:DispatchQueue = DispatchQueue(label: "com.suolapeikko.broadcastqueue", attributes: [])
         
         bluetoothPeripheralManager = CBPeripheralManager(delegate: self, queue: beaconQueue, options: nil)
         data = Beacon(uuid: uuid, major: major, minor: minor, power: power)
     }
 
-    /// Broadcastsing iBeacon as Bluetooth advertisement
+    /// Broadcasting iBeacon as Bluetooth advertisement
     func startBroadcasting() {
         
         print("Broadcasting: UUID=\(data.uuid.uuidString), major=\(String(describing: data.major)), minor=\(String(describing: data.minor)), power=\(String(describing: data.power))")
@@ -47,10 +42,12 @@ class BeaconBroadcaster: NSObject, CBPeripheralManagerDelegate {
     
     /// Reacting to Bluetooth peripheral manager's state change
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-        
-        if(!(bluetoothPeripheralManager.state.rawValue == CBManagerState.poweredOn.rawValue)) {
-            print("In order to user this tool, Bluetooth must be switched on\n")
-            exit(EXIT_FAILURE)
+       
+        if(bluetoothPeripheralManager != nil) {
+            if(!(bluetoothPeripheralManager.state.rawValue == CBManagerState.poweredOn.rawValue)) {
+                print("In order to user this tool, Bluetooth must be switched on\n")
+                exit(EXIT_FAILURE)
+            }
         }
     }
 }
